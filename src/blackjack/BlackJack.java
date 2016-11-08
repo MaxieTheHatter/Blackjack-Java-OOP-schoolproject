338952c5754e5586c1ff9edea20fd102fd47cb74
MaxieTheHatter <maxnelson90@gmail.com>
2016-11-08 09:11:54 +0100

    Updated comments
    Updated comments to be in english rather than swedish

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -6,25 +6,25 @@ public class BlackJack {
         //intro
         System.out.println("Welcome to Blackjack!");
         
-        //skapa kortlek
-        Deck playingDeck = new Deck(); //importera klassen Deck
-        playingDeck.createFullDeck(); //skapa kortlek
-        playingDeck.shuffle(); //blanda kortlek
+        //create new deck
+        Deck playingDeck = new Deck();
+        playingDeck.createFullDeck();
+        playingDeck.shuffle();
         
-        //ge spelaren en kortlek
-        Deck playerDeck = new Deck(); // ge spelaren kort
-        Deck dealerDeck = new Deck(); // ge dealer kort
+        //give the player and dealer a deck
+        Deck playerDeck = new Deck();
+        Deck dealerDeck = new Deck();
         
-        double playerMoney = 100.00; //ge spelaren startkapital för spelet
+        double playerMoney = 100.00; //Give player a bank
         
         Scanner input = new Scanner(System.in);
         int totalPlays = 0;
         int totalWins = 0;
         
-        //while-loop för spelet
-        while(playerMoney > 0) //fortsätt spela sålänge spelaren har pengar kvar
+        //while-loop for playing the game
+        while(playerMoney > 0) //keep playing as long as the player has money left
         {
-            //Samla insats från spelare
+            //Ask player to place bet
             System.out.println("You have played "+totalPlays+" times, and won "+totalWins+" times");
             System.out.println("You have $"+playerMoney+", place bet plase.");
             double playerBet = input.nextDouble();
@@ -33,58 +33,57 @@ public class BlackJack {
                 System.out.println("You don't have enough money for that.");
             }
             
-            boolean endRound = false; //boolean för att hålla koll på när rundor slutar
+            boolean endRound = false; //variable to keep track of wether current round is going
             
-            //Starta spelet
-            //Spelare börjar med 2 kort
+            //Start the game, give the player 2 cards
             playerDeck.draw(playingDeck);
             playerDeck.draw(playingDeck);
             
-            //Dealer börjar med 2 kort
+            //Give the dealer 2 cards
             dealerDeck.draw(playingDeck);
             dealerDeck.draw(playingDeck);
             
-            while(true) //loop för att dra fler kort i spelet
+            while(true) //while-loop to keep drawing cards
             {
                 System.out.print("\nYour hand:");
                 System.out.print(playerDeck.toString());
                 System.out.println("\nYour hand value is currently: "+
                         playerDeck.cardsValue());
                 
-                //Visa dealerns hand, visa bara ett kort
+                //shows the dealers hand, show the first card and hides the others
                 System.out.println("Dealer hand: "+dealerDeck.getCard(0).toString()+
                         "[Hidden]");
                 
-                //Fråga om spelaren vill dra kort eller stanna
+                //Ask if player wants to hit or stand
                 System.out.println("Do you want to (1)hit or (2)stand?");
                 int response = input.nextInt();
                 
-                //De drar fler kort
+                //If player hits, draw more cards
                 if(response == 1)
                 {
                     playerDeck.draw(playingDeck);
                     System.out.println("You drew a: "+
                             playerDeck.getCard(playerDeck.deckSize()-1).toString());
                     
-                    //om total är över 21, förlust
+                    //If hand value is over 21, bust
                     if(playerDeck.cardsValue() > 21)
                     {
                         System.out.println("Bust. Your current value is: "+
                                 playerDeck.cardsValue());
-                        playerMoney -= playerBet; //spelare förlorar sin insats
+                        playerMoney -= playerBet; //Withdraw player bet from player bank
                         endRound = true;
                         break;
                     }
                 }
-                if(response == 2) //om spelare väljar att stanna, avbryt loop
+                if(response == 2) //If player stands, end the loop
                 {
                     break;
                 }
             }
             
-            //visa dealerns kkort
+            //Show dealers full hand
             System.out.println("Dealers hand: "+dealerDeck.toString());
-            //ta reda på om dealer har mer poäng än spelaren och om rundan är slut
+            //Check wether dealers value is higher than players and if game is still going
             if((dealerDeck.cardsValue() > playerDeck.cardsValue()) && 
                     endRound == false)
             {
@@ -92,7 +91,7 @@ public class BlackJack {
                 playerMoney -= playerBet;
                 endRound = true;
             }
-            //simpel AI, dealer drar kort tills värdet är 16 eller högre, sen stannar han
+            //dealer-AI, if hand value is 16 or lower, hit.
             while((dealerDeck.cardsValue() < 17) && endRound == false)
             {
                 dealerDeck.draw(playingDeck);
@@ -100,9 +99,9 @@ public class BlackJack {
                         dealerDeck.getCard(dealerDeck.deckSize()-1).toString());
             }
             
-            //visa värde på dealerns hand
+            //Show the value of dealers hand
             System.out.println("Dealer's hands current value is: "+dealerDeck.cardsValue());
-            //om dealerns total är över 21, vinst
+            //If dealer is bust, you win
             if((dealerDeck.cardsValue() > 21) && endRound == false)
             {
                 System.out.println("Dealer is bust, you win!");
@@ -110,14 +109,14 @@ public class BlackJack {
                 endRound = true;
                 totalWins++;
             }
-            // se om det är oavgjort
+            //check if it's a draw
             if((playerDeck.cardsValue() == dealerDeck.cardsValue()) && endRound == false)
             {
                 System.out.println("It's a draw!");
                 endRound = true;
                 totalPlays++;
             }
-            //kontroll om spelare vunnit
+            //Check if player won
             if((playerDeck.cardsValue() > dealerDeck.cardsValue()) && endRound == false)
             {
                 System.out.println("You win!");
@@ -131,7 +130,7 @@ public class BlackJack {
                 playerMoney -= playerBet;
                 endRound = true;
             }
-            //lägg tillbaka korten i kortleken
+            //put back cards into the deck
             playerDeck.moveAllToDeck(playingDeck);
             dealerDeck.moveAllToDeck(playingDeck);
             System.out.println("End of round.");
